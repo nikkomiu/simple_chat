a2a786ef47619bd6fe5fe256608db8a41ea1e59c
Nikko Miu <nikko.miu@nikkomiu.com>
2016-06-01 14:46:20 -0500

    Added Schema information to models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  room_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Message < ApplicationRecord
   belongs_to :room
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Room < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
